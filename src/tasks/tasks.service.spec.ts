b08779015a227b6610487f9af12b3f3bdc6e05e1
Felipe Aguilera Calderón <aguilerafelipe1@gmail.com>
2023-08-06 03:30:29 -0400

    feat: getTasks test

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { TaskRepository } from './tasks.repository';
+
+const mockTaskRespository = () => ({
+  getTasks: jest.fn(),
+});
+
+const mockUser = {
+  id: 'someId',
+  username: 'Felipe',
+  password: 'somePassword',
+  tasks: [],
+};
 
 describe('TasksService', () => {
   let service: TasksService;
+  let tasksRepository: jest.Mocked<TaskRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mockTaskRespository },
+      ],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
+    tasksRepository = module.get(TaskRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and return the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue([]);
+      const result = await service.getTasks(null, mockUser);
+      expect(result).toEqual([]);
+    });
   });
 });
